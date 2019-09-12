aefc93eff94ddd99770442cd319c37d24681d8d9
George Trammell <gtrammell20@choate.edu>
2019-09-12 14:29:16 -0400

    new project: text based game

---
+++ TextBasedProject.py
@@ -0,0 +1,10 @@
+# File: TextBasedProject.py
+# Name: George Trammell
+# Start Date: 9/12/19
+# End Date: 
+# Desc:
+# Sources: None
+# On my honor, I have neither given nor received unauthorized aid.
+"""
+Ideas: Monopoly? Card game? 
+"""
