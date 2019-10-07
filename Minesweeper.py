cf8329914c42abeb3076b23cfbef7812339c3339
George Trammell <gtrammell20@choate.edu>
2019-10-06 20:17:03 -0400

    minesweeper pt 2

--- Minesweeper.py
+++ Minesweeper.py
@@ -20,9 +20,40 @@ def increase_around_mines(x, y, board):
             if board[r][c] != '*':
                 board[r][c] += 1
 
-def print_board(board):
+def print_answer_board(board):
     for i in board[1:-1]:
         print(*i[1:-1])
+
+def print_player_board(board):
+    for i in board[1:-1]:
+        playerBoard = ['៙' for i in solution]
+        print(*playerBoard[1:-1])
+
+# def create_player_board(board):
+#     for row in playerBoard:
+#         print(*playerBoard)
+        
+
+def print_errorcheck(board):
+    for i in board:
+        print(*i)
+
+def turn():
+    userInput = input('>>> ').split()
+    print(playerBoard[2])
+    print(playerBoard)
+    print(playerBoard[int(userInput[1])][int(userInput[0])])
+    if len(userInput) == 2:
+        # reveal
+        pass
+    elif len(userInput) == 3:
+        # flag
+        if playerBoard[int(userInput[1])][int(userInput[0])] == '៙':
+            playerBoard[int(userInput[1])][int(userInput[0])] == '⚐'
+        elif solution[int(userInput[1])][int(userInput[0])] == '⚐':
+            solution[int(userInput[1])][int(userInput[0])] == '៙'
+        
+
 #---------------------
 # Main Code
 #---------------------
@@ -34,4 +65,8 @@ mines = int(sys.argv[3])
 solution = [[0]*width for i in range(height)]
 
 add_mines(solution)
-print_board(solution)
+print_answer_board(solution)
+print_player_board(solution)
+# print_errorcheck(solution)
+turn()
+print_player_board(solution)
